8faaeb27048ac2b8597bf925949756ca52d3f352
Kendall Strautman <kendallstrautman@gmail.com>
2020-04-21 16:50:23 -0700

    Step 9: registers GitHub form

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,20 @@ import Head from 'next/head'
  ** Import helpers and GetStaticProps type
  */
 import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
+import { useGithubJsonForm } from 'react-tinacms-github'
 import { GetStaticProps } from 'next'
 
 export default function Home({ file }) {
-  const data = file.data
+  const formOptions = {
+    label: 'Home Page',
+    fields: [{ name: 'title', component: 'text' }],
+  }
+
+  /*
+   ** Register a JSON Tina Form
+   */
+  const [data, form] = useGithubJsonForm(file, formOptions)
+
   return (
     <div className="container">
       <Head>
