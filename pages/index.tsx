9be1f02757916ebb97a8077988cbafe822f7aa50
Kendall Strautman <kendallstrautman@gmail.com>
2020-07-07 09:03:53 -0700

    fix: register form

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,7 @@ export default function Home({ file, preview }) {
    ** Register a JSON Tina Form
    */
   const [data, form] = useGithubJsonForm(file, formOptions)
+  usePlugin(form)
 
   useGithubToolbarPlugins()
 
